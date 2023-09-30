e142906491122c945361c3a163e0021c99c4474b
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-09-30 09:02:01 +0200

    start new executable - converter convcp

---
+++ converter.cpp
@@ -0,0 +1,11 @@
+int main(int argc, char *argv[]) {
+    /*Args args(argc, argv);
+    if (argc < 2)
+        printHelp();
+    else {
+        args.parse();
+        if (args.error())
+            printHelp();
+        else convert(args.inFile, args.outFile, args.inFormat, args.outFormat);
+    }*/
+}
